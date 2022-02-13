eec30250904a9917f18306bac40021cc815c9b65
yongjulejule <lyjshow200@gmail.com>
2022-02-13 16:42:55 +0900

    :sparkles: feat : cpp00 ex00 finish

---
+++ cpp00/ex00/src/megaphone.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file megaphone.cpp
+ * @author yongjule
+ * @date 2022-02-13
+ */
+
+#include <iostream>
+#include <stdlib.h>
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char** argv) {
+	if (argc == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+
+	for (int i = 1; i < argc; i++) {
+		std::string str(argv[i]);
+		const int length = str.length();
+
+		for (int j = 0; j < length; j++)
+			str[j] = toupper(str[j]);
+		std::cout << str << " ";
+	}
+	std::cout << std::endl;
+	return 0;
+}
