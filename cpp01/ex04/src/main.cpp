176abe591dcc59aa2f4c03fa45753fd09de04536
yongjulejule <lyjshow200@gmail.com>
2022-02-19 15:38:19 +0900

    :sparkles: feat : ex04 error handling

--- cpp01/ex04/src/main.cpp
+++ cpp01/ex04/src/main.cpp
@@ -1,9 +1,9 @@
 /**
  * @file main.cpp
  * @author yongjule (lyjshow200@gmail.com)
- * @brief get file name
+ * @brief
  * @version 0.1
- * @date 2022-02-18
+ * @date 2022-02-19
  *
  * @copyright Copyright (c) 2022
  *
@@ -13,16 +13,38 @@
 #include <fstream>
 #include <string>
 
+ /**
+	* @brief  read file until meet EOF and return the content as std::string
+	*
+	* @param filePath /path/to/file
+	* @return std::string
+	*/
+const std::string readFileIntoString(const std::string& filePath) {
+	if (filePath[filePath.size() - 1] == '/' || filePath == "." || filePath == "..") {
+		std::cerr << "Error : " << filePath << " Is a directory\n";
+		exit(EXIT_FAILURE);
+	}
+
+	std::ifstream src;
+
+	src.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	try {
+		src.open(filePath, std::ios::in);
+		return std::string((std::istreambuf_iterator<char>(src)), std::istreambuf_iterator<char>());
+	}
+	catch (std::ifstream::failure& e) {
+		std::string state = src.rdstate() == std::ios::failbit ? "failbit!" : "badbit!";
+		std::cerr << "Error : Caught an ios_base::failure : " << e.what() << "\nstd::ios rdstatus : " << state << "\n";
+		exit(EXIT_FAILURE);
+	}
+}
+
 int main(int argc, char** argv) {
 	if (argc != 4) {
-		std::cout << "Invalid Arguments\n";
+		std::cout << "Error : Invalid Arguments\n";
 		return EXIT_FAILURE;
 	}
-	std::ifstream src;
-	src.open(argv[1], std::ios::in);
-	if (!src.is_open()) {
-		return EXIT_FAILURE;
-	}
-	std::string content;
-	std::cout << src.read(content, 1000) << std::endl;
+	const std::string content = readFileIntoString(argv[1]);
+	std::cout << content;
+	return EXIT_SUCCESS;
 }
