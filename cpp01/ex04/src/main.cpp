c565bfe05ee7be7e2feb4927d7cf5c564daf3fc9
yongjulejule <lyjshow200@gmail.com>
2022-02-20 11:36:02 +0900

    :sparkles: feat : ex04 make output file

--- cpp01/ex04/src/main.cpp
+++ cpp01/ex04/src/main.cpp
@@ -1,7 +1,7 @@
 /**
  * @file main.cpp
  * @author yongjule (lyjshow200@gmail.com)
- * @brief
+ * @brief replace content in file and save to filename.replace
  * @version 0.1
  * @date 2022-02-19
  *
@@ -13,12 +13,6 @@
 #include <fstream>
 #include <string>
 
- /**
-	* @brief  read file until meet EOF and return the content as std::string
-	*
-	* @param filePath /path/to/file
-	* @return std::string
-	*/
 const std::string readFileIntoString(const std::string& filePath) {
 	if (filePath[filePath.size() - 1] == '/' || filePath == "." || filePath == "..") {
 		std::cerr << "Error : " << filePath << " Is a directory\n";
@@ -26,15 +20,41 @@ const std::string readFileIntoString(const std::string& filePath) {
 	}
 
 	std::ifstream src;
-
 	src.exceptions(std::ifstream::failbit | std::ifstream::badbit);
 	try {
 		src.open(filePath, std::ios::in);
-		return std::string((std::istreambuf_iterator<char>(src)), std::istreambuf_iterator<char>());
+		src.seekg(0, std::ios::end);
+		const long long size = src.tellg();
+		src.seekg(0, std::ios::beg);
+		std::string content(size, '\0');
+		src.read(&content[0], size);
+		return content;
 	}
 	catch (std::ifstream::failure& e) {
 		std::string state = src.rdstate() == std::ios::failbit ? "failbit!" : "badbit!";
-		std::cerr << "Error : Caught an ios_base::failure : " << e.what() << "\nstd::ios rdstatus : " << state << "\n";
+		std::cerr << "Error : readFileIntoString : Caught an ios_base::failure : " << e.what() << "\nstd::ios rdstatus : " << state << "\n";
+		exit(EXIT_FAILURE);
+	}
+}
+
+const std::string replaceContent(const std::string& from, const std::string& to, const std::string& content) {
+	std::string newContent = content;
+	newContent.find(from);
+	newContent.find(to);
+	std::cout << newContent << std::endl;
+	return newContent;
+}
+
+void	saveToNewFile(const std::string& filePath, const std::string& newContent) {
+	std::ofstream out;
+	out.exceptions(std::ofstream::failbit | std::ofstream::badbit);
+	try {
+		out.open(filePath + ".replace", std::ios::out | std::ios::trunc);
+		out << newContent;
+	}
+	catch (std::ofstream::failure& e) {
+		std::string state = out.rdstate() == std::ios::failbit ? "failbit!" : "badbit!";
+		std::cerr << "Error : saveToNewFile : Caught an ios_base::failure : " << e.what() << "\nstd::ios rdstatus : " << state << "\n";
 		exit(EXIT_FAILURE);
 	}
 }
@@ -43,8 +63,12 @@ int main(int argc, char** argv) {
 	if (argc != 4) {
 		std::cout << "Error : Invalid Arguments\n";
 		return EXIT_FAILURE;
+	} else if (!*argv[1] || !*argv[2] || !*argv[3]) {
+		std::cout << "Error : Arguments cannot be empty\n";
 	}
 	const std::string content = readFileIntoString(argv[1]);
+	const std::string newContent = replaceContent(argv[2], argv[3], content);
+	saveToNewFile(argv[1], newContent);
 	std::cout << content;
 	return EXIT_SUCCESS;
 }
