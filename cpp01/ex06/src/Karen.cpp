2c2de12c3e1bbc37264b6fd2a325b2134f9d223c
yongjulejule <lyjshow200@gmail.com>
2022-02-20 17:57:41 +0900

    :sparkles: feat : finish ex06

---
+++ cpp01/ex06/src/Karen.cpp
@@ -0,0 +1,58 @@
+/**
+ * @file Karen.cpp
+ * @author yongjule (lyjshow200@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-02-20
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <iostream>
+#include "Karen.hpp"
+
+Karen::Karen(void) { }
+
+Karen::Karen(const Karen& src) {
+	*this = src;
+}
+
+Karen::~Karen(void) { }
+
+void Karen::debug(void) {
+	std::cout << "[ DEBUG ]" << std::endl;
+}
+
+void Karen::info(void) {
+	std::cout << "[ INFO ]" << std::endl;
+}
+
+void Karen::warning(void) {
+	std::cout << "[ WARNING ]" << std::endl;
+}
+
+void Karen::error(void) {
+	std::cout << "[ ERROR ]" << std::endl;
+}
+
+void Karen::complain(std::string level) {
+	int levelFlag = 0;
+	std::string levelList[4] = { "debug", "info", "warning", "error" };
+
+	for (;levelFlag < 4; levelFlag++) {
+		if (levelList[levelFlag] == level) break;
+	}
+	switch (levelFlag) {
+		case DEBUG: debug();
+		case INFO: info();
+		case WARNING: warning();
+		case ERROR: {
+				error();
+				break;
+			}
+		default: {
+				std::cout << "Invalid Level\n";
+			}
+	}
+}
---
+++ cpp01/ex06/src/main.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file main.cpp
+ * @author yongjule (lyjshow200@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-02-20
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <iostream>
+#include "Karen.hpp"
+
+int main(void) {
+	Karen karen;
+	std::cout << "======== debug =========" << std::endl;
+	karen.complain("debug");
+	std::cout << "======== info =========" << std::endl;
+	karen.complain("info");
+	std::cout << "======== warning =========" << std::endl;
+	karen.complain("warning");
+	std::cout << "======== error =========" << std::endl;
+	karen.complain("error");
+	return 0;
+}
