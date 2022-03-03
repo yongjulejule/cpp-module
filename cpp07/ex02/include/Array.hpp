e4d06883654189d1663e290925e644d87a8ff21f
yongjulejule <lyjshow200@gmail.com>
2022-03-03 21:31:04 +0900

    :sparkles: feat : [Cpp07Ex02] Fin :tada:

---
+++ cpp07/ex02/include/Array.hpp
@@ -0,0 +1,54 @@
+/**
+ * @file Array.hpp
+ * @author yongjule (lyjshow200@gmail.com)
+ * @brief Array template class
+ * @version 0.1
+ * @date 2022-03-03
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+#include <iostream>
+
+template <typename T>
+class Array {
+ private:
+  unsigned int _size;
+  T *_arr;
+
+ public:
+  Array<T>(void) : _size(0), _arr(new T[0]){};
+  Array<T>(unsigned int n) : _size(n), _arr(new T[n]){};
+  Array(const Array &src) : _size(0), _arr(NULL) { *this = src; };
+
+  Array &operator=(const Array &src) {
+    if (this->_arr) {
+      delete[] this->_arr;
+    }
+    const unsigned int srcSize = src.getSize();
+    this->_arr = new T[src._size];
+    for (unsigned int idx = 0; idx < srcSize; idx++) {
+      this->_arr[idx] = src._arr[idx];
+    }
+    this->_size = src._size;
+    return *this;
+  };
+
+  T &operator[](const unsigned int idx) {
+    if (idx > this->_size) throw Array<T>::OutOfRangeException();
+    return this->_arr[idx];
+  };
+  unsigned int getSize(void) const { return this->_size; };
+  class OutOfRangeException : public std::exception {
+    const char *what() const throw() { return "Array: Index is out of range."; }
+  };
+
+  ~Array(void) {
+    if (this->_arr) delete[] this->_arr;
+  };
+};
+
+#endif  // ARRAY_HPP
---
+++ cpp07/ex02/src/main.cpp
@@ -0,0 +1,70 @@
+/**
+ * @file main.cpp
+ * @author yongjule (lyjshow200@gmail.com)
+ * @brief Array template class test
+ * @version 0.1
+ * @date 2022-03-03
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <iostream>
+
+#include "Array.hpp"
+#define MAX_VAL 750
+
+void myTests(int argc, char** argv) {
+  Array<int> arr(10);
+  arr[0] = 10;
+  std::cout << arr[0] << std::endl;
+  Array<std::string> args(argc);
+  for (int i = 0; i < argc; i++) {
+    args[i] = argv[i];
+  }
+
+  for (int i = 0; i < argc; i++) {
+    std::cout << args[i] << std::endl;
+  }
+}
+
+int main(int argc, char** argv) {
+  Array<int> numbers(MAX_VAL);
+  int* mirror = new int[MAX_VAL];
+  srand(time(NULL));
+  for (int i = 0; i < MAX_VAL; i++) {
+    const int value = rand();
+    numbers[i] = value;
+    mirror[i] = value;
+  }
+  // SCOPE
+  {
+    Array<int> tmp = numbers;
+    Array<int> test(tmp);
+  }
+
+  for (int i = 0; i < MAX_VAL; i++) {
+    if (mirror[i] != numbers[i]) {
+      std::cerr << "didn't save the same value!!" << std::endl;
+      return 1;
+    }
+  }
+  try {
+    numbers[-2] = 0;
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << '\n';
+  }
+  try {
+    numbers[MAX_VAL] = 0;
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << '\n';
+  }
+
+  for (int i = 0; i < MAX_VAL; i++) {
+    numbers[i] = rand();
+  }
+  delete[] mirror;  //
+
+  myTests(argc, argv);
+  return 0;
+}
