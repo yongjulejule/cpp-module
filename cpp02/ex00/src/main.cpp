fb45de7e6fe6edad523d00812bd5b7d0606be020
yongjulejule <lyjshow200@gmail.com>
2022-03-07 22:19:54 +0900

    :recycle: refactor : [Cpp02] Fail in eval.. refactoring

--- cpp02/ex00/src/main.cpp
+++ cpp02/ex00/src/main.cpp
@@ -24,5 +24,9 @@ int main(void) {
   std::cout << b.getRawBits() << std::endl;
   std::cout << c.getRawBits() << std::endl;
 
+  Fixed d;
+  d.setRawBits(0x7fffff);
+  std::cout << d.getRawBits() << std::endl;
+
   return 0;
 }
